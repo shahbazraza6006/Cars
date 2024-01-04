77afa52048d16de010f030d386356f6ef58e7744
shahbazraza6006 <i201761@nu.edu.pk>
2024-01-04 05:40:07 +0500

    Update

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ function App() {
 
   return (
     <div className="App">
-      <BrowserRouter>
+      <BrowserRouter basename="/">
         <Routes>
           <Route exact path="/login" element={<Login />} />
           <Route exact path="/signup" element={<Signup />} />
